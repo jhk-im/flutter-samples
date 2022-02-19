58087019738b8ca59fd2863bb08690265e6ac531
jhk-im <kjhun200@gmail.com>
2022-02-20 02:09:21 +0900

    docs: animations_clone

--- animations/README.md
+++ animations/README.md
@@ -1,16 +1,66 @@
 # animations_clone
 
-A new Flutter project.
+## flutter 구성
 
-## Getting Started
+### Packages
 
-This project is a starting point for a Flutter application.
+#### pubspec.yaml
 
-A few resources to get you started if this is your first Flutter project:
+node.js = package.json / java = build.gradle 과 같은 패키지 의존성 관리
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+```yaml
+name: animations_clone
+descriptions : A new Flutter project.
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# 앱 버전 및 빌드 번호 정의
+# 버전은 점으로 구분된 3개의 숫자로 구분되며 +1 과 같이 빌드번호를 표시 
+version: 1.0.0+1
+
+environment:
+  sdk: ">=2.12.0 <3.0.0"
+
+dependencies:
+  flutter:
+    sdk: flutter
+    
+  # Cupertino 아이콘 폰트 추가
+  # IOS 스타일 아이콘으로 CupertinoIcons 클래스를 통해 사용
+  cupertino_icons: ^1.0.2
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^1.0.0
+
+flutter:
+  # Material 디자인 추가
+  # Android 스타일 아이콘 / Material 클래스를 통해 사용 
+  uses-material-design: true
+  assets:
+    - assets/
+```
+
+---
+
+#### analysis_options.yaml
+
+- 린트 규칙을 설정하여 개발속도 향상
+- const 를 활용하여 컴파일 타임을 늘려 런타임 줄임
+- required 같은 예약어들의 에러를 컴파일 타임에서 발견할 수 있음 
+
+```yaml
+include: ../analysis_options.yaml
+
+linter:
+  rules:
+    # Required because of the numbered sources in  lib/src/basics
+    file_names: false
+```
+
+---
+
+#### project_name.iml
+
+안드로이드 스튜디오에서 생성한 프로젝트 내부 파일 
+
+---
--- animations/lib/main.dart
+++ animations/lib/main.dart
@@ -11,8 +11,12 @@ void main() => runApp(const AnimationSamples());
 class Demo {
   final String name;
   final String route;
+  // widget 생성
+  // ex StatelessWidget.build or State.build
   final WidgetBuilder builder;
 
+  // Demo 인스턴스 생성자
+  // required 는 인스턴스 생성 시 필수로 입력해야하는 인자값에 지정
   const Demo({
     required this.name,
     required this.route,
@@ -27,16 +31,29 @@ final basicDemos = [
       builder: (context) => const AnimatedContainerDemo()),
 ];
 
+// Map<K, V>class
+// Map.fromEntries = Map 을 만들고 모든 MapEntry 를 추가한다.
 final basicDemoRoutes =
 Map.fromEntries(basicDemos.map((d) => MapEntry(d.route, d.builder)));
 
+// routes 정의
+// MaterialApp 생성자에는 routes 를 정의할 수 있음
+// Map<String, WidgetBuilder> -> route + name 에서 경로를 만들어 builder 함수로 매핑
+// 해당 맵을 사용하여 onGenerateRoute callback 생
 final allRoutes = <String, WidgetBuilder>{
   ...basicDemoRoutes,
 };
 
+// StatelessWidget -> 상태가 없는 widget
+// 변경 가능한 상태가 필요하지 않음
+// 이벤트, 사용자 액션에 상관없이 위젯이 로드될 때 한번만 렌더링 됨
 class AnimationSamples extends StatelessWidget {
   const AnimationSamples({Key? key}) : super(key: key);
 
+  // MaterialApp
+  // Flutter App UI 기본 구조를 잡아주는 App Widget
+  // 앱 이름, 테마, 다국어 지원 등 앱 설정 관리
+  // home 속성에 Start UI 지정
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,6 +62,8 @@ class AnimationSamples extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
       ),
       routes: allRoutes,
+      // 앱의 기본 경로인 Navigator.defaultRouteName 에 대한 위젯
+      // initialRoute 가 지정되지 않은 경우 먼저 표시되는 경로
       home: const HomePage(),
     );
   }
@@ -53,6 +72,9 @@ class AnimationSamples extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  // Scaffold
+  // Material Design 의 기본 구조가 되는 위젯
+  // Appbar, 중앙 스크린, bottom navigation 구조가 미리 정의된 위젯
   @override
   Widget build(BuildContext context) {
     final headerStyle = Theme.of(context).textTheme.headline6;
@@ -75,6 +97,9 @@ class DemoTile extends StatelessWidget {
 
   const DemoTile({required this.demo, Key? key}) : super(key: key);
 
+  // ListTile
+  // 아이콘, 체크박스 등과 같은 다른 위젯과 함께 선택적으로 1 ~ 3줄의 텍스트 포함
+  // 일반적으로 ListView, Drawers, Cards Columns 에 정렬 됨
   @override
   Widget build(BuildContext context) {
     return ListTile(
